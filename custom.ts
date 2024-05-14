f793e56ded58b7d227dda30f3360a3de9b12904a
Matt Baker <mbaker@itrsgroup.com>
2024-05-14 19:59:29 +0100

    Renamed initialised check

--- custom.ts
+++ custom.ts
@@ -54,7 +54,16 @@ namespace controller {
      * Checks if the controller has been set up
      */
     //% block group="Setup"
+    //% deprecated=true
     export function controllerIsInitialised(): boolean {
+        return _initialised
+    }
+
+    /**
+     * Checks if the controller has been set up
+     */
+    //% block group="Setup"
+    export function controllerInitialised(): boolean {
         return _initialised
     }
 
